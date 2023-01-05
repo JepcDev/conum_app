d050c7efe0adeacd817dfa71d0cef7c6f7fe80ee
Jepc <joseenrique.pc21@gmail.com>
2023-01-05 16:28:01 -0400

    ADD prefer const

--- lib/main.dart
+++ lib/main.dart
@@ -6,20 +6,27 @@ import 'package:flutter/material.dart';
 
 void main() {
   // dentro de main se corre la app de flutter
-  runApp(MyApp());
+  runApp( const MyApp());
 
 }
 
+// widget
 class MyApp extends StatelessWidget {
-  // si se tubiera valores aqui en esta lases los StatelessWidget no matendria esos valores
-  // si se tubiera valores aqui en esta lases los StatefulWidget estos si matendria esos valores
-  //todo widget o en este caso todo StatelessWidget tiene un metodo build que recibe un BuildContext y retorna un widget
+  const MyApp({super.key});
+
+  // si se tubiera valores aqui en esta clases los StatelessWidget no matendria esos valores
+  // si se tubiera valores aqui en esta clases los StatefulWidget estos si matendria esos valores
+  //* todo widget o en este caso todo StatelessWidget tiene un metodo build que recibe un BuildContext y retorna un widget
   @override
   Widget build(BuildContext context) {
     // Tenermos que regresar cualquier cosa que sea un widgets
-    return MaterialApp(
-      // el home es un forma de trabajar y nos pide nuestro widget que queremos mostrar aqui
+    //* Si tenemos alguna variable o widget que no cambia su valor desde que fue creado es mejor desclararlas como constantes
+    //* Es mejor ponerle const al ancestro mas alto al widget padre que engloba a los demas widgets
+    return const MaterialApp(
+      // el widget home es un forma de trabajar y nos pide nuestro widget que queremos mostrar aqui
+      // Center es otro widget de material que pone en el centro el contenido del child
       home: Center(
+        // Text es otro widget
         child: Text('Hola Mundo'),
       )
     );
