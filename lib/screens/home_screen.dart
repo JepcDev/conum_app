fc01bd5ae74993c37bfcc1f2fd98c3ad8500890c
Jepc <joseenrique.pc21@gmail.com>
2023-01-06 22:52:46 -0400

    ADD BuildContext y Scaffold

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,11 +9,20 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Es algo que mantiene la referencia de toda la aplicasion hasta este punto
+    // Sirve para saber en que o cual es el contexto en el cual nuestro widget esta siendo construido
+    // El buildContext es el arbol de todos los widgets y sabe donde se lo es solicitado, usado, instanciado cada widget
     
-    return const Center(
-        // Text es otro widget
-        child: Text('HomeScreen'),
-      );
+    // basicamente el scafold es contenedor que puede ser desplazado por el navbar o el footer
+    // Es un lienzo listo con las opciones para empezar a poner los widget necesarios
+    // Si ponemos algo en este contexto todo lo anterior a este no sabe que existe solo los widget que estan desntro despues de la creacion de ese contexto
+    return const Scaffold(
+      // body es el cuerpo de la vista
+      body: Center(
+          // Text es otro widget
+          child: Text('HomeScreen'),
+        ),
+    );
   }
 
 }
--- lib/screens/info.md
+++ lib/screens/info.md
@@ -2,4 +2,4 @@
 - Este directorio contiene todas las vistas o pantallas de la app.
 - Se aconseja poner en lower camel case los nombres de los archivos.
 - Las clases se nombran con upper camel case
-- Separar la logica en widgets independientes que realizen funciones especificas o especializadas asi es mas facil de mantener y de leer el codigo
+- Separar la contruccion de widgets en widgets independientes o mas pequeños que realizen funciones especificas o especializadas asi es mas facil de mantener y de leer el codigo
