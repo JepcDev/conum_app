bb059dae2607770f8dba50329bda55c0419a3fd9
Jepc <joseenrique.pc21@gmail.com>
2023-01-16 22:24:22 -0400

    ADD Sending functions as arguments

--- lib/screens/counter_screen.dart
+++ lib/screens/counter_screen.dart
@@ -10,6 +10,23 @@ class CounterScreen extends StatefulWidget {
 
 class _CounterScreenState extends State<CounterScreen> {
   int counter = 0;
+
+  // se necesita promero crear el widget para poder acceder a esta funcion
+  void increase() {
+    counter++;
+    setState(() {});
+  }
+
+  void decrease() {
+    counter--;
+    setState(() {});
+  }
+
+  void reset(){
+    counter = 0;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     const fontSize30 = TextStyle(fontSize: 30);
@@ -43,40 +60,65 @@ class _CounterScreenState extends State<CounterScreen> {
           FloatingActionButtonLocation.centerFloat,
 
       // DEV floatingActionButton
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          FloatingActionButton(
-            child: const Icon(Icons.exposure_minus_1_outlined),
-            onPressed: () {
-              counter--;
-              // setState es una funcion que notifica que ubo un cambio en el estado y como conseucencia se redibujara el widget con los nuevos cambios
-              // para mantener una mejor lectura de code primero se hacen los cambios luego se manda a llamar a l setState
-              setState(() {});
-            },
-          ),
-
-          // const SizedBox(width: 20,),
-
-          FloatingActionButton(
-            child: const Icon(Icons.restore_outlined),
-            onPressed: () {
-              counter = 0;
-              setState(() {});
-            },
-          ),
-
-          // const SizedBox(width: 20,),
-
-          FloatingActionButton(
-            child: const Icon(Icons.exposure_plus_1_outlined),
-            onPressed: () {
-              counter++;
-              setState(() {});
-            },
-          ),
-        ],
+      floatingActionButton: CustomFloatingActions(
+        // Se manda la referencia de la funcion no se la ejecuta
+        increaseFn: increase,decreaseFn: decrease,resetFn: reset,
       ),
+    );
+  }
+}
+
+class CustomFloatingActions extends StatelessWidget {
+
+  // increaseFn sera la accion que tendra el boton
+  // Es un valor se va a recibir y no se cambiara
+  // No puede ser constante por que va a cambiar cuando la app este ejecutandose
+  final Function increaseFn;
+  final Function decreaseFn;
+  final Function resetFn;
+  const CustomFloatingActions({
+    Key? key, required this.increaseFn, required this.decreaseFn, required this.resetFn,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        FloatingActionButton(
+          child: const Icon(Icons.exposure_minus_1_outlined),
+          onPressed: () => decreaseFn(),
+          // onPressed: () => setState(() => counter--),
+            // setState es una funcion que notifica que ubo un cambio en el estado y como conseucencia se redibujara el widget con los nuevos cambios
+            // para mantener una mejor lectura de code primero se hacen los cambios luego se manda a llamar al setState
+        ),
+
+        // const SizedBox(width: 20,),
+
+        FloatingActionButton(
+          // El onPress mada a llamar a una funcion que es el "setState"  y el "setState" internamente manda a llamar al "counter"
+          // El onPressed es una funcion que retorna el resultado, respuesta resolucion de la funcion llada "setState" que a su vez ejecuta el cambio de estado
+          // onPressed: () => resetFn(),
+          onPressed: () {
+            resetFn();
+          },
+          child:const Icon( Icons.restore_outlined ),
+          // onPressed: () => setState(() => counter = 0)
+          // onPressed: () {
+          //   counter++;
+          //   setState(() {});
+          // },
+        ),
+
+        // const SizedBox(width: 20,),
+
+        FloatingActionButton(
+          // onPressed: increaseFn(),
+          onPressed: () => increaseFn(),
+          child: const Icon(Icons.exposure_plus_1_outlined),
+          // onPressed: () => setState(() => counter++),
+        ),
+      ],
     );
   }
 }
